f60729e48170a8307b2cc48c4d292356654365aa
achwek <achwek.issawi@gmail.com>
2022-12-11 19:22:04 +0100

    commit v2

---
+++ src/Car_Manager/Car.java
@@ -0,0 +1,13 @@
+package Car_Manager;
+
+public class Car {
+
+	public int idCar;
+	public String typeCar;
+	public String modelCar;
+	
+	public Car() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/Car_Manager/Car_Manger.java
+++
@@ -1,9 +0,0 @@
-package Car_Manager;
-
-public class Car_Manger {
- public int ID;
-	public Car_Manger() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
